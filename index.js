2ebd45627d1600e13ae18d4b7873d36efe16f689
Idris <idris.maps@gmx.ch>
2016-02-20 11:39:23 +0100

    moved everything to svg

--- index.js
+++ index.js
@@ -1,40 +1,6 @@
-var getProj = require('./lib/get-proj')
-var Lines = require('./lib/Lines')
-var Polygons = require('./lib/Polygons')
-var Markers = require('./lib/Markers')
-var Labels = require('./lib/Labels')
-var background = require('./lib/background')
+var Svg = require('./lib/svg/index')
 
-module.exports = function(divId, w, h, bbox) {
-	var self = this
-	self.id = Date.now()
-	self.div = document.getElementById(divId)	
-	self.size = {width: w, height: h}
-	self.div.innerHTML = '<svg '
-		+ 'width="' + self.size.width + '" '
-		+ 'height="' + self.size.height + '" '
-		+ 'id="' + self.id
-		+'" ></svg>'
-	self.bbox = bbox
-	self.proj = getProj(self.size, self.bbox)
-	self.layerId = 0
-	self.newLayerId = function() { self.layerId = self.layerId + 1; return self.layerId }
-	self.textPositions = []
-	self.background = function(color) { background(color, self.id, self.size) }
-	self.markers = function(col, url, style) {
-		var x = new Markers(self.newLayerId(), col.features, url, style, self)
-		return x
-	}
-	self.labels = function(col, prop, style, transform) {
-		var x = new Labels(self.newLayerId(), col.features, prop, style, transform, self)
-		return x
-	}
-	self.lines = function(col, style) {
-		var x = new Lines(self.newLayerId(), col.features, style, self)
-		return x
-	}
-	self.polygons = function(col, style) {
-		var x = new Polygons(self.newLayerId(), col.features, style, self)
-		return x
-	}
+exports.svg = function(divId, w, h, bbox) {
+	var m = new Svg(divId, w, h, bbox)
+	return m
 }
