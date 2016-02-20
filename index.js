2ba728c0ca04ee3e0384b49dcd413e261f246506
Idris <idris.maps@gmx.ch>
2016-02-20 11:50:02 +0100

    getCollectionBbox()

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 var Svg = require('./lib/svg/index')
+var bbox = require('idris-geojson-bbox')
 
 exports.svg = function(divId, w, h, bbox) {
 	var m = new Svg(divId, w, h, bbox)
 	return m
+}
+
+exports.getCollectionBbox = function(col, callback) {
+	bbox(col, function(bb) {
+		callback(bb)
+	})
 }
