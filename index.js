588fac1e702426f7686d5a1707004c3b2863b30a
Idris <idris.maps@gmx.ch>
2016-02-22 11:57:10 +0100

    canvas

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 var Svg = require('./lib/svg/index')
+var Canvas = require('./lib/canvas/index')
 var bbox = require('idris-geojson-bbox')
 
 exports.svg = function(divId, w, h, bbox) {
 	var m = new Svg(divId, w, h, bbox)
+	return m
+}
+
+exports.canvas = function(divId, w, h, bbox) {
+	var m = new Canvas(divId, w, h, bbox)
 	return m
 }
 
